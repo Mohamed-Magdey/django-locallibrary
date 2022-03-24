b707d5452005b617565b597f198ef77b0bd568cc
Mohamed-Magdey <ragabmohame6@gmail.com>
2022-03-24 11:49:30 +0200

    Creat book_list and book_detail pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
+from pyexpat import model
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance
 
 def index(request):
@@ -23,3 +25,10 @@ def index(request):
 
   # Render the HTML template index.html with the data in the context variable
   return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+  model = Book
+  paginate_by = 10
+  
+class BookDetailView(generic.DetailView):
+  model = Book
